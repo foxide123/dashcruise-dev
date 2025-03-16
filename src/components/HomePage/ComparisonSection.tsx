e522e5cd3aac0895263596bb6a1a2657264660aa
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2025-03-16 22:06:56 +0100

    Adding global styles to header and list items

--- src/components/HomePage/ComparisonSection.tsx
+++ src/components/HomePage/ComparisonSection.tsx
@@ -13,8 +13,8 @@ export default function ComparisonSection() {
             <h3 className="font-medium text-sm leading-5 my-3.5 tracking-normal text-carrot-500">
               Why Choose Us
             </h3>
-            <h1 className="header">Build Faster, Rank Higher</h1>
-            <h1 className="header">Stand Out from Crowd</h1>
+            <h1 className="header-custom">Build Faster, Rank Higher</h1>
+            <h1 className="header-custom">Stand Out from Crowd</h1>
           </div>
 
           <p className="font-normal text-base leading-6 my-4">
@@ -31,22 +31,22 @@ export default function ComparisonSection() {
                 </h2>
               </div>
               <ul>
-                <li className="list-item">
+                <li className="list-item-custom">
                   <CheckIcon /> Optimized for Google Search Engine
                 </li>
-                <li className="list-item">
+                <li className="list-item-custom">
                   <CheckIcon /> Ultrafast Load Times
                 </li>
-                <li className="list-item">
+                <li className="list-item-custom">
                   <CheckIcon /> Secure
                 </li>
-                <li className="list-item">
+                <li className="list-item-custom">
                   <CheckIcon /> Unique
                 </li>
-                <li className="list-item">
+                <li className="list-item-custom">
                   <CheckIcon /> Adaptable to your needs
                 </li>
-                <li className="list-item">
+                <li className="list-item-custom">
                   <CheckIcon /> Expert Assistance
                 </li>
               </ul>
@@ -59,22 +59,22 @@ export default function ComparisonSection() {
                 </h2>
               </div>
               <ul>
-                <li className="list-item">
+                <li className="list-item-custom">
                   <XIcon /> Worse SEO
                 </li>
-                <li className="list-item">
+                <li className="list-item-custom">
                   <XIcon /> Slow
                 </li>
-                <li className="list-item">
+                <li className="list-item-custom">
                   <XIcon /> Security Risks from Plugins
                 </li>
-                <li className="list-item">
+                <li className="list-item-custom">
                   <XIcon /> Limited by themes
                 </li>
-                <li className="list-item">
+                <li className="list-item-custom">
                   <XIcon /> Common Designs
                 </li>
-                <li className="list-item">
+                <li className="list-item-custom">
                   <XIcon /> Poor / No Support
                 </li>
               </ul>
