c0d1852b932c7932b747b929f2866d4b4912e840
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2025-03-18 22:25:35 +0100

    Creating Highlight Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import ComparisonSection from "@/components/HomePage/sections/ComparisonSection";
 import HeroSection from "@/components/HomePage/sections/HeroSection";
+import HighlightSection from "@/components/HomePage/sections/HighlightSection";
 import PortfolioSection from "@/components/HomePage/sections/PortfolioSection";
 import PricingSection from "@/components/HomePage/sections/PricingSection";
 
@@ -9,6 +10,7 @@ export default function Home() {
     <div>
       <HeroSection />
       <ComparisonSection />
+      <HighlightSection />
       <PortfolioSection />
       <PricingSection />
     </div>
