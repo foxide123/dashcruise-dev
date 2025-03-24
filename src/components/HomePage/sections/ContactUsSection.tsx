907bbc0c7eb9ec2b34b89c8f64da393b80b55d93
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2025-03-24 19:58:28 +0100

    Contact Us Section

--- src/components/HomePage/sections/ContactUsSection.tsx
+++ src/components/HomePage/sections/ContactUsSection.tsx
@@ -1,25 +1,49 @@
-"use client"
-import { handleFormSubmition } from "@/app/actions"; 
+"use client";
+import { handleFormSubmition } from "@/app/actions";
 
 type ContactUsProps = {
-    lg_screen_width: string;
-    default_screen_width: string;
-}
+  lg_screen_width: string;
+  default_screen_width: string;
+};
 
 export default function ContactUsSection({
-    lg_screen_width,
-    default_screen_width
-}: ContactUsProps){
-
-
-    return (
-        <div className="w-screen flex items-center justify-center bg-white">
-            <div className={`${lg_screen_width} ${default_screen_width} border-2 border-carrot-500`}>
-                <h1>Contact Us</h1>
-                <form action={handleFormSubmition}>
-                <input type="text" id="todo" name="todo" required />
-                </form>
-            </div>
-        </div>
-    );
+  lg_screen_width,
+  default_screen_width,
+}: ContactUsProps) {
+  return (
+    <div className="w-screen flex items-center justify-center bg-amber-200 pb-40">
+      <div
+        className={`mt-10 ${lg_screen_width} ${default_screen_width} rounded-lg border-2 border-carrot-500 bg-carrot-200`}
+      >
+        <h1 className="text-3xl font-medium text-center pt-5">Contact Us</h1>
+        <form action={handleFormSubmition} className="flex flex-col p-10">
+          <input
+            type="text"
+            id="nameInput"
+            name="name"
+            required
+            placeholder="Please enter name..."
+            className="border-1 p-5 outline-none"
+          />
+          <input
+            type="email"
+            id="emailInput"
+            name="email"
+            required
+            placeholder="Please enter email..."
+            className="border-1 mt-5 p-5 outline-none"
+          />
+          <textarea
+            name="message"
+            required
+            placeholder="Please write the message..."
+            className="border-1 mt-5 p-5 h-50 outline-none"
+          />
+          <button type="submit" className="cursor-pointer text-2xl rounded-xl bg-carrot-500 mt-5 p-5">
+            Send
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
