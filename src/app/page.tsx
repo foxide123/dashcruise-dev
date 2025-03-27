79ffe3576011d496529b6a1cc68ea6c5edbd104b
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2025-03-27 13:49:48 +0100

    Uncommenting QAWrapper and ContactUsWrapper in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ import ComparisonSection from "@/components/HomePage/sections/ComparisonSection"
 import HighlightSection from "@/components/HomePage/sections/HighlightSection";
 
 import PricingSection from "@/components/HomePage/sections/PricingSection";
-{
-  /*
+
+
 import ClientOnly from "@/components/ClientOnly";
 
 import QAWrapper from "@/components/HomePage/sections/QA/QAWrapper";
 import ContactUsWrapper from "@/components/HomePage/sections/ContactUs/ContactUsWrapper";
-*/
-}
+
+
 export default function Home() {
   const lg_screen_width = "lg:w-[75vw]";
   const default_screen_width = "w-[85vw]";
@@ -32,11 +32,12 @@ export default function Home() {
         lg_screen_width={lg_screen_width}
         default_screen_width={default_screen_width}
       />
-      {/* <PortfolioSection
+      {/*
+       <PortfolioSection
           lg_screen_width="lg:w-[75vw]"
           default_screen_width="w-[85vw]"
-        />*/}
-      {/*
+        />
+      */}
         <ClientOnly>
           <QAWrapper
             lg_screen_width={lg_screen_width}
@@ -47,7 +48,6 @@ export default function Home() {
             default_screen_width={default_screen_width}
           />
         </ClientOnly>
-*/}
       <PricingSection
         lg_screen_width={lg_screen_width}
         default_screen_width={default_screen_width}
