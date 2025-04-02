5fe1e19dd5aba0d2236c72fd004d9e9533850547
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2025-04-02 03:27:41 +0200

    Adding switch and modifying the layout order of components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
-
 import HeroSection from "@/components/HomePage/sections/HeroSection";
 
 import ComparisonSection from "@/components/HomePage/sections/ComparisonSection";
 
 import HighlightSection from "@/components/HomePage/sections/HighlightSection";
 
-import PricingSection from "@/components/HomePage/sections/PricingSection";
-
+import PricingSection from "@/components/HomePage/sections/Pricing/PricingSection";
 
 import ClientOnly from "@/components/ClientOnly";
 
 import QAWrapper from "@/components/HomePage/sections/QA/QAWrapper";
 import ContactUsWrapper from "@/components/HomePage/sections/ContactUs/ContactUsWrapper";
 import PortfolioSection from "@/components/HomePage/sections/PortfolioSection";
-
+import MeetingScheduleSection from "@/components/HomePage/sections/MeetingSchedule/MeetingScheduleSection";
 
 export default function Home() {
   const lg_screen_width = "lg:w-[75vw]";
@@ -32,26 +30,32 @@ export default function Home() {
         lg_screen_width={lg_screen_width}
         default_screen_width={default_screen_width}
       />
-      
-       <PortfolioSection
-          lg_screen_width="lg:w-[75vw]"
-          default_screen_width="w-[85vw]"
+
+      <PortfolioSection
+        lg_screen_width="lg:w-[75vw]"
+        default_screen_width="w-[85vw]"
+      />
+
+      <ClientOnly>
+        <ContactUsWrapper
+          lg_screen_width={lg_screen_width}
+          default_screen_width={default_screen_width}
         />
-      
-        <ClientOnly>
-          <QAWrapper
-            lg_screen_width={lg_screen_width}
-            default_screen_width={default_screen_width}
-          />
-          <ContactUsWrapper
-            lg_screen_width={lg_screen_width}
-            default_screen_width={default_screen_width}
-          />
-        </ClientOnly>
+      </ClientOnly>
       <PricingSection
         lg_screen_width={lg_screen_width}
         default_screen_width={default_screen_width}
       />
+      <MeetingScheduleSection
+        lg_screen_width={lg_screen_width}
+        default_screen_width={default_screen_width}
+      />
+      <ClientOnly>
+        <QAWrapper
+          lg_screen_width={lg_screen_width}
+          default_screen_width={default_screen_width}
+        />
+      </ClientOnly>
       {/*  <CookieBanner />*/}
     </div>
   );
